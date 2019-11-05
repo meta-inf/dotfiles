da31c852d7a347716726a6b06d373f5a354d7d2c
Ziyu Wang <wzy196@gmail.com>
2019-11-05 16:40:23 +0800

    Update setup-conda.sh

--- setup-conda.sh
+++ setup-conda.sh
@@ -1,7 +1,9 @@
 set -e
-wget https://mirrors.tuna.tsinghua.edu.cn/anaconda/archive/Anaconda3-5.1.0-Linux-x86_64.sh
-chmod +x Anaconda3-5.1.0-Linux-x86_64.sh
+if [ ! -e ~/Anaconda3-5.1.0-Linux-x86_64.sh ]; then
+    wget https://mirrors.tuna.tsinghua.edu.cn/anaconda/archive/Anaconda3-5.1.0-Linux-x86_64.sh -P $HOME
+fi
+chmod +x ~/Anaconda3-5.1.0-Linux-x86_64.sh
 ln -s "$(pwd)/.condarc" ~/.condarc
-bash Anaconda3-5.1.0-Linux-x86_64.sh -b -p $HOME/anaconda3
+bash ~/Anaconda3-5.1.0-Linux-x86_64.sh -b -p $HOME/anaconda3
 # echo "pip install tensorflow-gpu" > /tmp/_setup.sh
 # bash /tmp/_setup.sh
