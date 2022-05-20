e7a10a577068a1dbd78905c62ab3f7e19ef1752e
meta-inf <wzy196@gmail.com>
2022-05-20 14:11:35 +0800

    s

--- ag.py
+++ ag.py
@@ -1,5 +1,13 @@
 #! /usr/bin/env python
-import numpy
-import sys
-a = list(map(float, sys.stdin.readlines()))
-print(numpy.mean(a), numpy.std(a), numpy.std(a)/(len(a)-1)**0.5)
+import numpy as np
+import sys, argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-t', type=str, default='')
+args = parser.parse_args()
+
+arr = list(map(float, sys.stdin.readlines()))
+if args.t:
+  arr = [eval(args.t) for a in arr]
+print('mean / sd / ci / n', np.mean(arr), np.std(arr), np.std(arr)/(len(arr)-1)**0.5*1.96, len(arr))
+print('p50/25/75', np.percentile(arr, [50,25,75]))
